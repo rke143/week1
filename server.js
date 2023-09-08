6df4e7b2151a1384fe0abdb0e0904c2490c6133d
Julia Vrn <julia.voronetskaja@tktk.ee>
2023-09-08 13:22:30 +0200

    week 1 code

---
+++ server.js
@@ -0,0 +1,10 @@
+let firstName = 'Harry';
+let lastName = "Potter";
+//template literals
+//console.log(`Hello, ${firstName}!`);
+
+let greeting = `Hello, ${firstName} ${lastName}!`;
+console.log(greeting);
+
+
+
